6886ee2a80c60cf0e4e43b3737cee12c9f4bfb0c
shaminabd <shynggysabdullayev@gmail.com>
2022-11-07 22:28:29 +0500

    commit

--- Python course/Basic_principles_pyhtonlang/1_2.py
+++ Python course/Basic_principles_pyhtonlang/1_2.py
@@ -1,1 +1,9 @@
-print(123123)
+objects = [1, 2, 1, 2, 3]
+ans = 0
+tmp = []
+for obj in objects: # доступная переменная objects
+    if (not(obj in tmp)):
+        ans += 1
+        tmp.append(obj)
+
+print(ans)
